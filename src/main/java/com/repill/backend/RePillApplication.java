53b3bca02bf59ef8fd34bb6481f7ef531db6c3b0
Seungwon-Choi <bestwondev@gmail.com>
2025-05-17 21:57:57 +0900

    ✏️ Chore: 스프링 부트 초기 설정

---
+++ src/main/java/com/repill/backend/RePillApplication.java
@@ -0,0 +1,13 @@
+package com.repill.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RePillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RePillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/repill/backend/RePillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repill.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RePillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
