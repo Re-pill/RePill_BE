1340f8bd9e28a6c9652faa55c63f92f61df0de4e
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-05-18 13:12:09 +0900

    ✨ Feat: Rank, LocationType enum 클래스 필드 추가

--- src/main/java/com/repill/backend/medicineboxarea/entity/LocationType.java
+++ src/main/java/com/repill/backend/medicineboxarea/entity/LocationType.java
@@ -2,5 +2,9 @@ package com.repill.backend.medicineboxarea.entity;
 
 public enum LocationType {
 
-    // TODO 장소 유형 작성
+    PHARMACY,        // 약국
+    COMMUNITY_CENTER, // 주민센터
+    HEALTH_CENTER,   // 보건소
+    HOSPITAL,        // 병원
+    LOCAL_GOV_COLLECTION_CENTER // 지자체 수거소
 }
