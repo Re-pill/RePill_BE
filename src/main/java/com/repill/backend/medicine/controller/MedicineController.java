1d3abf3d1ccd5a94d0527b0b8f99c5c61efb1ec4
Seungwon-Choi <bestwondev@gmail.com>
2025-05-20 11:20:29 +0900

    ✨ Feat: Swagger 문서화를 위해 @Operation 어노테이션 추가 #10

--- src/main/java/com/repill/backend/medicine/controller/MedicineController.java
+++ src/main/java/com/repill/backend/medicine/controller/MedicineController.java
@@ -5,6 +5,7 @@ import com.repill.backend.apiPayload.code.status.SuccessStatus;
 import com.repill.backend.medicine.dto.MedicineRequest;
 import com.repill.backend.medicine.dto.MedicineResponse;
 import com.repill.backend.medicine.service.MedicineService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,18 +18,21 @@ public class MedicineController {
 
     private final MedicineService medicineService;
 
+    @Operation(summary = "약품 등록 API", description = "약품 정보를 등록합니다.")
     @PostMapping
     public ApiResponse<MedicineResponse.MedicineDetailResponse> createMedicine(@RequestBody @Validated MedicineRequest request) {
         MedicineResponse.MedicineDetailResponse response = medicineService.createMedicine(request);
         return ApiResponse.of(SuccessStatus._OK, response);
     }
 
+    @Operation(summary = "D-Day 리스트 조회 API", description = "회원의 폐기되지 않은 약품 D-Day 리스트를 조회합니다.")
     @GetMapping("/d-day/{memberId}")
     public ApiResponse<MedicineResponse.MedicineDDayListResponse> getDDayList(@PathVariable Long memberId) {
         MedicineResponse.MedicineDDayListResponse response = medicineService.getDDayList(memberId);
         return ApiResponse.of(SuccessStatus._OK, response);
     }
 
+    @Operation(summary = "약품 상세 조회 API", description = "약품의 상세 정보를 조회합니다.")
     @GetMapping("/{medicineId}")
     public ApiResponse<MedicineResponse.MedicineDetailResponse> getMedicineDetail(@PathVariable Long medicineId) {
         MedicineResponse.MedicineDetailResponse response = medicineService.getMedicineDetail(medicineId);
