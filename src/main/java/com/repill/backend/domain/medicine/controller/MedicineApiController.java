a94428288d61aa11e653b7b2b2b5a7ca2f9a2472
Seungwon-Choi <bestwondev@gmail.com>
2025-07-28 23:50:04 +0900

    refactor: MemberController -> MemberApiController 컨트롤러명 변경 #64

--- src/main/java/com/repill/backend/domain/medicine/controller/MedicineApiController.java
+++ src/main/java/com/repill/backend/domain/medicine/controller/MedicineApiController.java
@@ -9,20 +9,19 @@ import com.repill.backend.domain.medicine.service.MedicineService;
 import com.repill.backend.global.security.handler.annotation.AuthUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/medicines")
 @Validated
 @RequiredArgsConstructor
-public class MedicineController implements MedicineApiDocs {
+public class
+MedicineApiController implements MedicineApiDocs {
 
     private final MedicineService medicineService;
 
     @Override
+    @PostMapping
     public ApiResponse<MedicineResponse.MedicineDetailResponse> createMedicine(@AuthUser Long memberId,
                                                                                @RequestBody @Validated MedicineRequest request) {
         MedicineResponse.MedicineDetailResponse response = medicineService.createMedicine(memberId, request);
@@ -30,24 +29,28 @@ public class MedicineController implements MedicineApiDocs {
     }
 
     @Override
+    @GetMapping("/d-day")
     public ApiResponse<MedicineResponse.MedicineDDayListResponse> getDDayList(@AuthUser Long memberId) {
         MedicineResponse.MedicineDDayListResponse response = medicineService.getDDayList(memberId);
         return ApiResponse.of(SuccessStatus._OK, response);
     }
 
     @Override
+    @GetMapping("/{medicineId}")
     public ApiResponse<MedicineResponse.MedicineDetailResponse> getMedicineDetail(@PathVariable Long medicineId) {
         MedicineResponse.MedicineDetailResponse response = medicineService.getMedicineDetail(medicineId);
         return ApiResponse.of(SuccessStatus._OK, response);
     }
 
     @Override
+    @DeleteMapping("/{medicineId}")
     public ApiResponse<String> deleteMedicine(@PathVariable Long medicineId, @AuthUser Long memberId) {
         medicineService.deleteMedicine(medicineId, memberId);
         return ApiResponse.of(SuccessStatus._OK,"MY약 삭제가 왼료되었습니다.");
     }
 
     @Override
+    @PatchMapping("/{medicineId}")
     public ApiResponse<String> patchMedicine(@PathVariable Long medicineId,
                                              @AuthUser Long memberId,
                                              @RequestBody PatchMedicineRequest medicineRequest) {
